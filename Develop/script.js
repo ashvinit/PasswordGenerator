7ba942b86a92ece2d1cd6fbe1ac74a957b3bcf0d
Ashvini Thomas <ashvinithomas@gmail.com>
2020-10-27 18:07:00 -0400

    added all requirements to confirm

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,3 @@
-// When Generate Password button is clicked, the first prompt should appear. 
-function startPrompt(){
-  var numberOfCharacters = prompt("How many characters would you like your password to contain?");
-}
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -13,6 +8,24 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+// When Generate Password button is clicked, the first prompt should appear. 
+function startPrompt(){
+  var numberOfCharacters = prompt("How many characters would you like your password to contain?");
+
+  //Confirm including special characters
+  var specialCharacters = confirm("Click OK to confirm including special chaacters");
+
+  //Confirm including numeric characters
+  var numericCharacters = confirm("Click OK to confirm including numeric characters");
+
+  //Confirm including lowercase characters
+  var lowercaseCharacters = confirm("Click OK to confirm including lowercase letters");
+
+  //Confirm including uppercase characters
+  var uppercaseCharacters = confirm("Click OK to confirm including uppercase letters");
 
 }
 
