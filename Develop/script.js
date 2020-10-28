a0d734e758caf48b50449a490b00fd9a869b9167
Ashvini Thomas <ashvinithomas@gmail.com>
2020-10-27 20:07:43 -0400

    array for arrays created

--- Develop/script.js
+++ Develop/script.js
@@ -20,6 +20,9 @@ var numbers = [0,1,2,3,4,5,6,7,8,9];
 
 var symbols = ['!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[','{', ']', '}', '|', ';', ':', '<', ',', '>', '.', '?', '~', '`'];
 
+//all conditions array
+var characters = [uppercaseLetters, lowecaseLetters, numbers, symbols];
+
 
 // When Generate Password button is clicked, the first prompt should appear. 
 function startPrompt(){
